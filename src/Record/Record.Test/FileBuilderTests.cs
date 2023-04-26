e6da54f1bb783b7992d3473bffa9f2fc83733188
henriette.rebnor <henriette.rebnor@bouvet.no>
2023-04-26 15:44:34 +0200

    FileBuilder has been replaced by FileRecordBuilder, now returns a Record instead of a list of triples

--- src/Record/Record.Test/FileBuilderTests.cs
+++ src/Record/Record.Test/FileBuilderTests.cs
@@ -1,35 +1,25 @@
 ﻿using System.Text;
 using FluentAssertions;
-using Record = Records.Immutable.Record;
 
 namespace Records.Tests;
 
 public class FileBuilderTests
 {
     [Fact]
-    public void FileBuilder_ShouldCreate_ValidRecordContent()
+    public void FileRecordBuilder_ShouldCreate_ValidRecordWithFileContent()
     {
-        var attachmentRecordId = "https://example.com/recordId";
-        var fileName = "B123-EX-W-LA-XLSX";
 
-        var file = new FileBuilder()
-            .WithId(attachmentRecordId)
+        var fileRecord = new FileRecordBuilder()
+            .WithId("https://example.com/fileRecordId")
+            .WithIsSubRecordOf("https://example.com/superRecordId")
+            .WithScopes("ex:scope1", "ex:scope2", "ex:scope3")
             .WithMediaType("xlsx")
-            .WithFileName(fileName)
+            .WithFileName("B123-EX-W-LA-XLSX")
             .WithLanguage("en-US")
-            .WithContent(Encoding.UTF8.GetBytes("This is very cool file content"))
+            .WithFileContent(Encoding.UTF8.GetBytes("This is very cool file content"))
             .Build();
 
-        var attachmentRecord = new RecordBuilder()
-            .WithId(attachmentRecordId)
-            .WithContent(file)
-            .WithDescribes(attachmentRecordId)
-            .WithScopes("ex:scope")
-            .Build();
-
-        file.Should().NotBeNull();
-        attachmentRecord.Should().NotBeNull();
-        attachmentRecord.QuadsWithPredicate(Namespaces.FileContent.HasTitle).Select(q => q.Object).FirstOrDefault().Should().Be(fileName);
+        fileRecord.Should().NotBeNull();
     }
 }
 
