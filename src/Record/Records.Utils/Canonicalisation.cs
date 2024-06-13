d1eb96827b129d78011516d994979d2192d5c464
Markus AP <89122836+markus-ap@users.noreply.github.com>
2024-06-13 16:15:17 +0200

    added utils

---
+++ src/Record/Records.Utils/Canonicalisation.cs
@@ -0,0 +1,24 @@
+﻿using VDS.RDF;
+
+namespace Records.Utils;
+
+public static class CanonicalisationExtensions
+{
+    public static ITripleStore Canonicalise(this ITripleStore store) => new RdfCanonicalizer().Canonicalize(store).OutputDataset;
+    public static IEnumerable<Triple> Canonicalise(this IEnumerable<Triple> triples)
+    {
+        var store = new TripleStore();
+        var graph = new Graph();
+        foreach (var triple in triples) graph.Assert(triple);
+        store.Add(graph);
+
+        return store.Canonicalise().Triples;
+    }
+
+    public static IGraph Canonicalise(this IGraph graph)
+    {
+        var store = new TripleStore();
+        store.Add(graph);
+        return store.Canonicalise().Graphs.Single();
+    }
+}
