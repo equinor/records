964b77fcc98611082abacf6d59bc65486f1276a6
Markus Andre Pedersen <89122836+markus-ap@users.noreply.github.com>
2024-06-18 15:30:57 +0200

    RDF canonicalisation of records (#72)
    Implemented `RdfCanonicalizer` for records. A record's string will now
    always be the canonicalised string.
    
    Added a method `SameCanonAs` which returns true whether both records'
    canonicalised strings are equal.
    
    ---------
    
    Co-authored-by: Dag Hovland <dhovl@equinor.com>

--- src/Record/Record.Test/ImmutableRecordTests.cs
+++ src/Record/Record.Test/ImmutableRecordTests.cs
@@ -482,6 +482,7 @@ public class ImmutableRecordTests
         // Assert
         result.Should().Be(record);
         result.SameTriplesAs(record).Should().BeTrue();
+        result.SameCanonAs(record).Should().BeFalse();
     }
 
     [Fact]
@@ -499,5 +500,16 @@ public class ImmutableRecordTests
         result.Should().HaveCount(1);
         result.First().Name.Should().NotBe(recordId);
     }
-}
 
+    [Fact]
+    public void Record_SameCanon_Is_True_With_Same_BlankNodes()
+    {
+        // Arrange
+        var recordId = "https://example.com/1";
+
+        var record = TestData.ValidRecord(recordId);
+        var record2 = TestData.ValidRecord(recordId);
+
+        record.SameCanonAs(record2).Should().BeTrue();
+    }
+}
