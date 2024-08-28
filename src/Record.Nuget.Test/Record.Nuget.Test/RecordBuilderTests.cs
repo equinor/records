4aaec72909296ef2da5864c9cba804c3cd678527
Embla Øye <embla.oye@bouvet.no>
2024-08-28 12:34:35 +0200

    Remove version test

--- src/Record.Nuget.Test/Record.Nuget.Test/RecordBuilderTests.cs
+++ src/Record.Nuget.Test/Record.Nuget.Test/RecordBuilderTests.cs
@@ -34,40 +34,6 @@ public class RecordBuilderTests
         record.Id.Should().Be(id);
     }
 
-    [Fact]
-    public void Can__Find__Version()
-    {
-        var id = CreateRecordId("0");
-        var scopes = CreateObjectList(2, "scope");
-        var describes = CreateObjectList(2, "describes");
-
-        var record = new RecordBuilder()
-            .WithScopes(scopes)
-            .WithDescribes(describes)
-            .WithId(id)
-            .Build();
-
-        var query = new SparqlQueryParser().ParseFromString(
-            $"SELECT * WHERE {{ graph <{record.Id}>  {{ <{record.Id}> <http://www.w3.org/ns/prov#wasGeneratedBy>/<http://www.w3.org/ns/prov#wasAssociatedWith> ?version . }} }}");
-
-        var tripleStore = record.TripleStore();
-        var ds = new InMemoryDataset((TripleStore)tripleStore);
-        var qProcessor = new LeviathanQueryProcessor(ds);
-        var qResults = qProcessor.ProcessQuery(query);
-
-        if (qResults is SparqlResultSet qResultSet)
-        {
-            var version = qResultSet.Results.Single(res => res["version"]
-                .ToString()
-                .Equals("https://github.com/equinor/records/commit/unknown", StringComparison.OrdinalIgnoreCase));
-            version.Should().NotBeNull();
-        }
-        else
-        {
-            throw new Exception("qResults is not a SparqlResultSet");
-        }
-    }
-
     private static string CreateRecordId(string id) => $"https://ssi.example.com/record/{id}";
 
     private static List<string> CreateObjectList(int numberOfObjects, string subset)
