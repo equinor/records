91de673826781d7c8a7b55e930ea0f3c06304945
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-03-17 13:28:48 +0100

    remove nullability on properties in records

--- src/Record/Record.Model/Immutable/Record.cs
+++ src/Record/Record.Model/Immutable/Record.cs
@@ -20,10 +20,10 @@ public class Record : IEquatable<Record>
     private LeviathanQueryProcessor _queryProcessor;
     private string _nQuadsString;
 
-    public List<Quad>? Metadata { get; private set; }
+    public List<Quad> Metadata { get; private set; }
     public HashSet<string> Scopes { get; private set; }
     public HashSet<string> Describes { get; private set; }
-    public List<string>? Replaces { get; private set; }
+    public List<string> Replaces { get; private set; }
 
     public string? IsSubRecordOf { get; set; }
 
